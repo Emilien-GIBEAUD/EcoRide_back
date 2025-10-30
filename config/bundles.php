897f351f8389d9463f8a96d2a91a30249d796a17
Emilien-GIBEAUD <emiliengibeaud@gmail.com>
2025-10-30 11:13:11 +0100

    Update compose.yaml and Dockerfile to add NoSQL DataBase (MongoDB and Mongo-express) and install mongodb and mongodb-odm-bundle with composer.

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
